0e25b79addd4493cc16e42da07d4454ffb60a2a2
r3921768-collab <r3921768@gmail.com>
2026-04-04 22:59:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import telebot
+import time
+import os
 
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+
+channel_username = "@your_channel_name"
+
+while True:
+    try:
+        bot.send_message(channel_username, "Hello bhai! Auto message 🚀")
+        time.sleep(60)
+    except Exception as e:
+        print(e)
+        time.sleep(10)
