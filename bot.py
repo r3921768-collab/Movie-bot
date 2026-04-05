b0cc45bafa090d42ede3f9eb1ab60d2cd497b340
r3921768-collab <r3921768@gmail.com>
2026-04-05 10:20:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,12 @@ import os
 from flask import Flask
 from threading import Thread
 
-bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+token = os.getenv("BOT_TOKEN")
+
+if not token:
+    raise Exception("BOT_TOKEN missing!")
+
+bot = telebot.TeleBot(token)
 channel_username = "@ITQ5AVAT3FYWNWJL"
 
 app = Flask(__name__)
@@ -19,14 +24,13 @@ def run_bot():
             bot.send_message(channel_username, "🔥 Free movies yaha milengi 🎬")
             time.sleep(60)
         except Exception as e:
-            print(e)
+            print("BOT ERROR:", e)
             time.sleep(10)
 
 if __name__ == "__main__":
-    # Flask pehle start karo (IMPORTANT)
-    port = int(os.environ.get("PORT", 10000))
-
-    # Bot background me
-    Thread(target=run_bot).start()
-
-    app.run(host="0.0.0.0", port=port)
+    try:
+        port = int(os.environ.get("PORT", 10000))
+        Thread(target=run_bot).start()
+        app.run(host="0.0.0.0", port=port)
+    except Exception as e:
+        print("MAIN ERROR:", e)
